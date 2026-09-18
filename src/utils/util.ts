1c66a6c8f91f8a52506ac588840ec3387bd5bd99
Amelith <136112098+Amelith@users.noreply.github.com>
2026-09-18 17:25:54 +0200

    kinda pointless icon guessing/conversion and half finished pointless creating website shortcuts

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -4,6 +4,10 @@ export function defineModule<T extends Arguments>(config: Module<T>): Module<T> 
   return config;
 }
 
+export function capitalize(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
 export function indentAllButFirstLine(str: string, indentAmount = 2): string {
   const [first, ...rest] = str.split('\n');
   if (rest.length === 0) return first ?? '';
