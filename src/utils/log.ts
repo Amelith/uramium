d0695c5db46a19d9bfa7e6d3c5b01abaec9c85ee
Amelith <136112098+Amelith@users.noreply.github.com>
2026-09-23 20:22:56 +0200

    fix log levels

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,43 +1,43 @@
 const LOG_LEVEL = process.env['LOG_LEVEL']?.toUpperCase() ?? 'INFO';
 
 const LOG_LEVELS = {
-  off: 0,
-  error: 1,
-  warn: 2,
-  log: 3,
-  info: 4,
-  debug: 5,
-  trace: 6,
+  OFF: 0,
+  ERROR: 1,
+  WARN: 2,
+  LOG: 3,
+  INFO: 4,
+  DEBUG: 5,
+  TRACE: 6,
 }
 
-const LOG_LEVEL_NUM = (LOG_LEVEL in LOG_LEVELS) ? LOG_LEVELS[LOG_LEVEL as keyof typeof LOG_LEVELS] : LOG_LEVELS.info;
+const LOG_LEVEL_NUM = (LOG_LEVEL in LOG_LEVELS) ? LOG_LEVELS[LOG_LEVEL as keyof typeof LOG_LEVELS] : LOG_LEVELS.INFO;
 
 export function trace(msg: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.trace) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.TRACE) return;
   console.trace(msg);
 }
 
 export function debug(msg: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.debug) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.DEBUG) return;
   console.debug(msg);
 }
 
 export function info(msg: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.info) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.INFO) return;
   console.info(msg);
 }
 
 export function log(msg: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.log) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.LOG) return;
   console.log(msg);
 }
 
 export function warn(msg: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.warn) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.WARN) return;
   console.warn(msg);
 }
 
 export function error(msg: string, err: unknown): void {
-  if (LOG_LEVEL_NUM < LOG_LEVELS.error) return;
+  if (LOG_LEVEL_NUM < LOG_LEVELS.ERROR) return;
   console.error(msg, err);
 }
